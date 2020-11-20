961d9393f58c4b0fd45bda5529caf352a3114884
Amir <bbbzzj@icloud.com>
2020-11-20 23:53:14 +0300

    create test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,3 +10,9 @@ def test_v2(app, client):
     res = client.get('/v2')
     assert res.status_code == 200
     assert res.data == b"Second action"
+
+
+def test_abdusalimov(app, client):
+    res = client.get('/abdusalimov')
+    assert res.status_code == 200
+    assert res.data == b"Hello from CI with GitHub Actions by Abdusalimov"
